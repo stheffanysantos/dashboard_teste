49f7074e86fc96a4997747aac50691c67d8e02f8
stheffany santos <stheffany.sts@gmail.com>
2025-01-30 18:08:33 -0300

    versão teste importação / dashboard leandro

--- start.sh
+++ start.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+# Atualiza o ambiente e instala o Python
+apt-get update && apt-get install -y python3-pip
+
+# Instala as dependências
+pip install -r requirements.txt
+
+# Inicia o servidor FastAPI
 uvicorn main:app --host 0.0.0.0 --port 8000
